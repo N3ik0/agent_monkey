bec0f502f353770fe2e7fecc739a4e1ec31c1643
N3ik0 <N3ik0@users.noreply.github.com>
2026-02-25 13:21:01 +0100

    feat(base_feature): ajout du modèle de base de feature

---
+++ features/base_feature.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+imports pandas as pd
+
+class BaseFeature(ABC):
+    """
+    Abstract Base Class for all feature engineering modules.
+    Ensures a standardized way to compute technical indicators, statistical 
+    transformations, or computer vision matrix formulations.
+    """
+
+    def __init__(self, name: str):
+        """
+        Initialize the feature module.
+        Args : 
+            name (str): The unique identifier for this feature (e.g: 'RSI_14)
+                        Used for tracking the notion
+        """
+        self.name = name
+
+    @abstractmethod
+    def compute(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Computes the feature and appends it to the market data.
+        
+        WARNING (Quant Rule): Do NOT introduce Look-ahead bias here. 
+        Never use future data (e.g., df['close'].shift(-1)) to compute a current feature.
+        
+        Args:
+            df (pd.DataFrame): The raw or partially processed OHLCV market data.
+            
+        Returns:
+            pd.DataFrame: A new DataFrame containing the original columns plus the computed feature.
+        """
+        pass
